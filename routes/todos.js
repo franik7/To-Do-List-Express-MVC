31f36b4f4522cd5a1d7669865f12e390598ed4e9
Frants Kavalionak <frants.kavalionak@gmail.com>
2023-07-06 22:22:50 -0400

    first commit

---
+++ routes/todos.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const router = express.Router()
+const todosController = require('../controllers/todos')
+
+router.get('/', todosController.getTodos)
+
+router.post('/createTodo', todosController.createTodo)
+
+router.put('/markComplete', todosController.markComplete)
+
+router.put('/markIncomplete', todosController.markIncomplete)
+
+router.delete('/deleteTodo', todosController.deleteTodo)
+
+router.delete('/deleteAll', todosController.deleteAll)
+
+module.exports = router
